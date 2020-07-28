91ad27f66dd7586e8b026f959628783345ef4083
AnkitDutta02 <67562481+AnkitDutta02@users.noreply.github.com>
2020-07-29 01:06:50 +0530

    Add files via upload
    Drums using keyboard too and drum animation

--- index.js
+++ index.js
@@ -1,43 +1,55 @@
 
 for(var i=0;i<document.querySelectorAll(".drum").length;i++)
 {
-document.querySelectorAll(".drum")[i].addEventListener("click",handleclick);
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+  clickOrPress(this.innerHTML);
+  animationOnPlay(this.innerHTML);
+});
+}
+document.addEventListener("keydown", function(event) {
+clickOrPress(event.key);
+animationOnPlay(event.key)
+});
+
+function clickOrPress(key)
+{
+  switch(key)
+  {
+  case "w":
+  var tom1= new Audio('sounds/tom-1.mp3');
+  tom1.play();
+  break;
+  case "a":
+  var tom2= new Audio('sounds/tom-2.mp3');
+  tom2.play();
+  break;
+  case "s":
+  var tom3= new Audio('sounds/tom-3.mp3');
+  tom3.play();
+  break;
+  case "d":
+  var tom4= new Audio('sounds/tom-4.mp3');
+  tom4.play();
+  break;
+  case "j":
+  var snare= new Audio('sounds/snare.mp3');
+  snare.play();
+  break;
+  case "k":
+  var crash= new Audio('sounds/crash.mp3');
+  crash.play();
+  break;
+  case "l":
+  var kickbass= new Audio('sounds/kick-bass.mp3');
+  kickbass.play();
+  break;
+}
 }
 
-function handleclick()
+function animationOnPlay(currentkey)
 {
-console.log(this.innerHTML);
-switch(this.innerHTML)
-{
-case 'w':
-var audio= new Audio('sounds/tom-1.mp3');
-audio.play();
-break;
-case 'a':
-var audio= new Audio('sounds/tom-2.mp3');
-audio.play();
-break;
-case 's':
-var audio= new Audio('sounds/tom-3.mp3');
-audio.play();
-break;
-case 'd':
-var audio= new Audio('sounds/tom-4.mp3');
-audio.play();
-break;
-case 'j':
-var audio= new Audio('sounds/snare.mp3');
-audio.play();
-break;
-case 'k':
-var audio= new Audio('sounds/crash.mp3');
-audio.play();
-break;
-case 'l':
-var audio= new Audio('sounds/kick-bass.mp3');
-audio.play();
-break;
-
-}
-
+  document.querySelector("."+currentkey).classList.add("pressed");
+  setTimeout(function () {
+    document.querySelector("."+currentkey).classList.remove("pressed")
+  },100);
 }
